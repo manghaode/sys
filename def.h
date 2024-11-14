901735cbdf8d35b8d7b7c4b7cbd26cdd153bf8aa
manghaode <manghaode@outlook.com>
2024-11-14 20:41:02 +0800

    define type

---
+++ def.h
@@ -0,0 +1,17 @@
+#ifndef DEF_H
+#define DEF_H
+#include <stdint.h>
+namespace sys {
+/// define unsigned int
+using u8  = uint8_t;
+using u16 = uint16_t;
+using u32 = uint32_t;
+using u64 = uint64_t;
+
+/// define int
+using i8  = int8_t;
+using i16 = int16_t;
+using i32 = int32_t;
+using i64 = int64_t;
+}
+#endif// DEF_H

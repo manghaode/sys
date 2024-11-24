2037c112eb2d341ebb2304d7a8deb4fade3faef1
huoweiyuan <huoweiyuan@outlook.com>
2024-11-24 09:42:44 +0800

    huoweiyuan/sys#1:新增自定义申请和释放

--- singleton.hh
+++ singleton.hh
@@ -1,24 +1,35 @@
 #ifndef SINGLETON_HH
 #define SINGLETON_HH
 
+#include <functional>
 #include <mutex>
 
 namespace sys {
 namespace utils {
-template <typename CN> struct Del_singleton {
+template <typename CN> struct Del_instance {
   CN *instance{nullptr};
-  ~Del_singleton() {
+  std::function<void(const CN *)> dealloctor;
+
+public:
+  Del_instance() = default;
+  Del_instance(std::function<void(const CN *)> dealloctor)
+      : dealloctor(dealloctor) {}
+  ~Del_instance() {
     if (instance) {
-      delete instance;
+      if (dealloctor != nullptr) {
+        dealloctor(instance);
+      } else {
+        delete instance;
+      }
       instance = nullptr;
     }
   }
 };
 
-template <typename CN, typename... Args> CN *get_singleton_instance(Args&&... args) {
+template <typename CN, typename... Args> CN *get_instance(Args&&... args) {
   static CN *_instance = nullptr;
   static std::mutex _mutex;
-  static Del_singleton<CN> _del;
+  static Del_instance<CN> _del;
   
   if (_instance == nullptr) {
     std::unique_lock<std::mutex> _lc(_mutex);
@@ -29,6 +40,25 @@ template <typename CN, typename... Args> CN *get_singleton_instance(Args&&... ar
   }
   return _instance;
 }
+
+template <typename CN, typename... Args>
+CN *get_instance_custom_alloc(std::function<CN *(Args &&...)> allocator,
+                              std::function<void(const CN *)> deallocator,
+                              Args &&...args) {
+  static CN *_instance = nullptr;
+  static std::mutex _mutex;
+  static Del_instance<CN> _del(deallocator);
+  
+  if (_instance == nullptr) {
+    std::unique_lock<std::mutex> _lc(_mutex);
+    if (_instance == nullptr) {
+      _instance = allocator(std::forward(args...));
+      _del.instance = _instance;
+    }
+  }
+  return _instance;
+}
+
 } // namespace utils
 } // namespace sys
 
